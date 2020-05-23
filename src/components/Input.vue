41704f6543812c907d8b8e26eb02c5df66cf6b67
5hamele55 <roman.zhuk1993@gmail.com>
2020-05-23 17:26:06 +0300

    commit

---
+++ src/components/Input.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-form ref="form">
+    <v-text-field
+      v-model="title"
+      label="Нова справа"
+      :rules="rule"
+      :counter="counterEn"
+      append-icon="mdi-plus"
+      @click:append="addTodo"
+    ></v-text-field>
+    <!-- <v-btn class="success mb-3" @click="addTodo">Додати</v-btn> -->
+  </v-form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counterEn: 50,
+      rule: [
+        v =>
+          (v.trim().length >= 5 && v.trim().length <= this.counterEn) ||
+          `Мін. довживна 5 смволів макс.- ${this.counterEn}`
+      ],
+      title: ""
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.$refs.form.validate()) {
+        const newTodo = {
+          id: Date.now(),
+          title: this.title,
+          complited: false
+        };
+        this.$emit("add-todo", newTodo);
+        this.title = "";
+      }
+    }
+  }
+};
+</script>
